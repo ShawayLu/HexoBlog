f60f3158adfab35375e38d1a3c28897c3f8b7c4a
ShawayL <741077606@qq.com>
2025-03-06 14:37:27 +0800

    优化switch.sh脚本，添加帮助信息并改进参数处理逻辑

--- switch.sh
+++ switch.sh
@@ -8,7 +8,14 @@ repogitee="git@gitee.com:Shaway/shaway.git"
 
 deployD=0
 
-while [ $# -gt 0 ]; do # Until you run out of parameters...
+print_help() {
+    echo "Options:"
+    echo " --git   [Switch script to deploy to git]"
+    echo " --gitee [Switch script to deploy to gitee]"
+    exit 0
+}
+
+while [ $# -gt 0 ]; do
     case "$1" in
         --git)
             deployD=1
@@ -17,21 +24,22 @@ while [ $# -gt 0 ]; do # Until you run out of parameters...
             deployD=2
             ;;
         --help)
-            echo "Options:"
-            echo " --git   [Switch script to deploy to git]"
-            echo " --gitee [Switch script to deploy to gitee]"
-            exit
+            print_help
+            ;;
+        *)
+            echo "Invalid option: $1"
+            print_help
             ;;
     esac
-    shift # Check next set of parameters.
+    shift
 done
 
-if [ "$deployD" -eq 0 ] ; then
-    echo "Error!please set deploy method"
+if [ "$deployD" -eq 0 ]; then
+    echo "Error! Please set deploy method"
     exit 1
 fi
 
-if [ "$deployD" -eq 1 ] ; then
+if [ "$deployD" -eq 1 ]; then
     url=$urlgit
     repo=$repogit
 else
@@ -39,13 +47,8 @@ else
     repo=$repogitee
 fi
 
+urlline=$(grep "^url:" ./_config.yml -n -m 1 | awk -F ":" '{print $1}')
+repoline=$(grep "^  repo:" ./_config.yml -n -m 1 | awk -F ":" '{print $1}')
 
-urlline=`grep "^url:" ./_config.yml -n -m 1 | awk -F ":" '{print $1}'`
-repoline=`grep "^  repo:" ./_config.yml -n -m 1 | awk -F ":" '{print $1}'`
-
-sed -i ""$urlline"c url: '"$url"'" ./_config.yml
-sed -i ""$repoline"c\ \ repo: "$repo"" ./_config.yml
-
-
-
-
+sed -i "${urlline}c url: '${url}'" ./_config.yml
+sed -i "${repoline}c\ \ repo: ${repo}" ./_config.yml
